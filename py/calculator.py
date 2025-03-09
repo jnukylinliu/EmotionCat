1b89d8885170ff1aaf00c51045f495a3f5f79ca1
jnukylinliu <jnukylinliu@126.com>
2025-03-09 12:40:54 +0800

    Add the prediction strategy

--- py/calculator.py
+++ py/calculator.py
@@ -1,37 +1,29 @@
 from strategy_func import get_score_volume,get_score_limitup,get_score_limitdown,get_score_updays,get_score_index
+from calculate_scores import calculate_total_scores
 
-# 计算权重
-w_volume = 0.25
-w_other = 0.25 * (1 - w_volume)
-w_limitup = w_other
-w_limitdown = w_other
-w_updays = w_other
-w_index = w_other
+
 
 # 输入数据
-volume = [17300, 16359, 16772,18198,17150,17991,17210,20805,18968]
-limit_up = [140, 84, 95,74,200,113,597,282,213]  
-limit_down = [2, 68, 17,244,94,699,164,157,85]  
-consecutive_up_days = [3, -1, 1,-1,1,-1,1,-1,-2]
-index_change = [0.56, -0.12, 0.85,-0.42,0.43,-0.93,0.81,-0.18,-0.8]
+volume = [17300, 16359, 16772,18198,17150,17991,17210,20805,18968,16247,14356,14938,19065,18182]
+limit_up = [140, 84, 95,74,200,113,597,282,213,339,396,287,448,236]
+limit_down = [2, 68, 17,244,94,699,164,157,85,169,94,41,26,126]
+consecutive_up_days = [3, -1, 1,-1,1,-1,1,-1,-2,-2,1,2,3,-1]
+index_change = [0.56, -0.12, 0.85,-0.42,0.43,-0.93,0.81,-0.18,-0.8,-0.12,0.22,0.53,1.17,-0.25]
 
 # 计算每一组的综合得分
-total_scores = []
-for v, lu, ld, cud, ic in zip(volume, limit_up, limit_down, consecutive_up_days, index_change):
-    score_volume = get_score_volume(v)
-    score_limitup = get_score_limitup(lu)
-    score_limitdown = get_score_limitdown(ld)
-    score_updays = get_score_updays(cud)
-    score_index = get_score_index(ic)
-    
-    total_score = (w_volume * score_volume +
-                   w_limitup * score_limitup -
-                   w_limitdown * score_limitdown -
-                   w_updays * score_updays +
-                   w_index * score_index)
-    
 
-    total_scores.append(round(total_score , 2))
+w_volume = 0  # 示例初始值
+w_limitup = 0
+w_limitdown = 0
+w_updays = 0
+w_index = 0
+
+
+total_scores = calculate_total_scores(volume, limit_up, limit_down, consecutive_up_days, index_change, 
+                                      w_volume, w_limitup, w_limitdown, w_updays, w_index)
+
+
+
 
 # 定义保存到txt的函数
 def save_emotion_values_to_txt(file_path, emotion_values):
--- py/readme.txt
+++ py/readme.txt
@@ -4,3 +4,44 @@ python
 pyinstaller --onefile your_script.py
 
 这样不需要安装环境
+
+加入预测：
+
+基于成交量
+
+如果成交量volume >20000
+	判断大盘涨跌天数consecutive_up_days, ，如果consecutive_up_days<0 //下跌
+		判断成交量相比上一个交易日变化，如果change>0
+			预测反弹
+		如果change<0
+			预测反弹
+	如果consecutive_up_days=1	//上涨
+		判断成交量相比上一个交易日变化，如果change>0  //放量上涨
+			预测继续涨
+		如果change<0    //缩量上涨
+			预测继续涨
+	如果consecutive_up_days>2	//上涨两天
+		判断成交量相比上一个交易日变化，如果change>0  //放量上涨
+			预测继续涨
+		如果change<0    //缩量上涨
+			预测继续涨
+如果成交量18000<volume<20000
+	判断大盘涨跌天数consecutive_up_days, ，如果consecutive_up_days<0 //下跌
+		判断成交量相比上一个交易日变化，如果change>0
+			预测反弹
+		如果change<0
+			预测反弹
+	如果consecutive_up_days=1	//上涨
+		判断成交量相比上一个交易日变化，如果change>0  //放量上涨
+			预测继续涨
+		如果change<0    //缩量上涨
+			预测继续涨
+	如果consecutive_up_days>2	//上涨两天
+		判断成交量相比上一个交易日变化，如果change>0  //放量上涨
+			预测继续涨
+		如果change<0    //缩量上涨
+			预测继续涨
+
+
+
+
